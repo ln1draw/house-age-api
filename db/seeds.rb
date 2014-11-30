c31bb4544e21ff61bc1b386c1416ba5f763297ea
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-29 16:24:36 -0800

    removing Sublime's tabs in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@ Property.destroy_all
 commercials = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CommBldg.csv")
 
 CSV.read(commercials).each do |n|
-	p = Property.new
-	p.parcel_number = n[0]
-	p.year_built = n[20]
-	p.address = n[4].gsub(/\s+/, " ")
-	p.property_type = "Commercial"
-	p.save
+  p = Property.new
+  p.parcel_number = n[0]
+  p.year_built = n[20]
+  p.address = n[4].gsub(/\s+/, " ")
+  p.property_type = "Commercial"
+  p.save
 end
 
 
@@ -22,12 +22,12 @@ end
 condos = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CondoComplex.csv")
 
 CSV.read(condos).each do |n|
-	p = Property.new
-	p.parcel_number = n[0]
-	p.year_built = n[14]
-	p.address = n[23].gsub(/\s+/, " ")
-	p.property_type = "Condo"
-	p.save
+  p = Property.new
+  p.parcel_number = n[0]
+  p.year_built = n[14]
+  p.address = n[23].gsub(/\s+/, " ")
+  p.property_type = "Condo"
+  p.save
 end
 
 
@@ -35,12 +35,12 @@ end
 apartments = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_AptComplex.csv")
 
 CSV.read(apartments).each do |n|
-	p = Property.new
-	p.parcel_number = n[0]
-	p.year_built = n[13]
-	p.address = n[20].gsub(/\s+/, " ")
-	p.property_type = "Apartment"
-	p.save
+  p = Property.new
+  p.parcel_number = n[0]
+  p.year_built = n[13]
+  p.address = n[20].gsub(/\s+/, " ")
+  p.property_type = "Apartment"
+  p.save
 end
 
 
@@ -48,10 +48,10 @@ end
 residentials = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_ResBldg.csv")
 
 CSV.read(residentials).each do |n|
-	p = Property.new
-	p.parcel_number = n[0]
-	p.year_built = n[43]
-	p.address = n[4].gsub(/\s+/, " ")
-	p.property_type = "Residential"
-	p.save
+  p = Property.new
+  p.parcel_number = n[0]
+  p.year_built = n[43]
+  p.address = n[4].gsub(/\s+/, " ")
+  p.property_type = "Residential"
+  p.save
 end
