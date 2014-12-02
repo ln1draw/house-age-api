8f9c0fbb4c398fb37803a4aa56a87bd783153566
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-01 22:15:34 -0800

    properties_controller_spec

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -2,12 +2,35 @@ require 'rails_helper'
 
 describe PropertiesController do
 
-  # describe 'subject' do
-  #
-  #   it 'does something' do
-  #
-  #   end
-  #
-  # end
+  let(:property) { Property.create(parcel_number: "0942000275", address: "1000 FOURTH AVE",
+                                   year_built: 2004, property_type: "Commercial") }
+
+  describe 'GET index' do
+
+    it 'is successful' do
+      get :index, format: :json
+      expect(response.status).to be 200
+    end
+
+    it 'renders index template' do
+      get :index, format: :json
+      expect(response).to render_template(:index)
+    end
+
+  end
+
+  describe 'GET show' do
+
+    it 'is successful' do
+      get :show, { id: property.id, format: :json }
+      expect(response.status).to be 200
+    end
+
+    it 'renders show template' do
+      get :show, { id: property.id, format: :json }
+      expect(response).to render_template(:show)
+    end
+
+  end
 
 end
