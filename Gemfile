d1bb48e84318752e07595f319f74709eee6d06a5
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-04 09:36:08 -0800

    adding progress details to seeds.rb

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ end
 
 gem 'simplecov', :require => false, :group => :test
 
+gem 'colorize'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,88 @@
 require 'open-uri'
 require 'csv'
+require 'colorize'
 
 # Remove all previous records
+puts "Removing old data (#{Property.count} Records Found)..."
 Property.destroy_all
 
 
-# Commercial Records
-commercials = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CommBldg.csv")
-
-CSV.read(commercials).each do |n|
-  p = Property.new
-  p.parcel_number = "#{n[0]}#{n[1]}"
-  p.year_built = n[20]
-  p.address = n[4].gsub(/\s+/, " ")
-  p.property_type = "Commercial"
-  p.save
-end
-
-
-# Condo Records
-condos = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CondoComplex.csv")
-
-CSV.read(condos).each do |n|
-  p = Property.new
-  p.parcel_number = "#{n[0]}0000"
-  p.year_built = n[14]
-  p.address = n[23].gsub(/\s+/, " ")
-  p.property_type = "Condo"
-  p.save
-end
-
-
 # Apartment Records
+puts "Parsing Apartments..."
 apartments = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_AptComplex.csv")
 
-CSV.read(apartments).each do |n|
+CSV.read(apartments).each_with_index do |n, i|
   p = Property.new
   p.parcel_number = "#{n[0]}#{n[1]}"
   p.year_built = n[13]
   p.address = n[20].gsub(/\s+/, " ")
   p.property_type = "Apartment"
   p.save
+  print ".".colorize(:light_blue) if i % 100 == 0
 end
+
+apartments.close
+puts "\nApartments Complete..."
+puts "[DB contains #{Property.count} records]\n\n"
+
+
+# Commercial Records
+puts "Parsing Commercial Buildings..."
+commercials = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CommBldg.csv")
+
+CSV.read(commercials).each_with_index do |n, i|
+  p = Property.new
+  p.parcel_number = "#{n[0]}#{n[1]}"
+  p.year_built = n[20]
+  p.address = n[4].gsub(/\s+/, " ")
+  p.property_type = "Commercial"
+  p.save
+  print ".".colorize(:magenta) if i % 100 == 0
+end
+
+commercials.close
+puts "\nCommercial Buildings Complete..."
+puts "[DB contains #{Property.count} records]\n\n"
+
+
+# Condo Records
+puts "Parsing Condos..."
+condos = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_CondoComplex.csv")
+
+CSV.read(condos).each_with_index do |n, i|
+  p = Property.new
+  p.parcel_number = "#{n[0]}0000"
+  p.year_built = n[14]
+  p.address = n[23].gsub(/\s+/, " ")
+  p.property_type = "Condo"
+  p.save
+  print ".".colorize(:green) if i % 100 == 0
+end
+
+condos.close
+puts "\nCondos Complete..."
+puts "[DB contains #{Property.count} records]\n\n"
 
 
 # Residential Records
+puts "Parsing Residential Buildings..."
 residentials = open("https://dl.dropboxusercontent.com/u/34984839/EXTR_ResBldg.csv")
 
-CSV.read(residentials).each do |n|
+CSV.read(residentials).each_with_index do |n, i|
   p = Property.new
   p.parcel_number = "#{n[0]}#{n[1]}"
   p.year_built = n[43]
   p.address = n[4].gsub(/\s+/, " ")
   p.property_type = "Residential"
   p.save
+  print ".".colorize(:yellow) if i % 100 == 0
 end
 
-duplicates = Property.select('parcel_number, count(parcel_number)').group('parcel_number').having('count(parcel_number) > 1')
-duplicates.destroy_all
+residentials.close
+puts "\nResidential Buildings Complete..."
+puts "[DB contains #{Property.count} records]\n\n"
+
+puts "Import Complete!"
+
+# duplicates = Property.select('parcel_number, count(parcel_number)').group('parcel_number').having('count(parcel_number) > 1')
+# duplicates.destroy_all
