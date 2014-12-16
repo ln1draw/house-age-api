0e1bc3fdd7df2698a31b92cadce9e4e6ed9d0f46
Kristina Hjertberg <hjert.k@gmail.com>
2014-12-16 11:13:38 -0800

    changing search sql query

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,20 @@
 class SearchesController < ApplicationController
 
   def index
-    if params[:address]
-      @results = Property.where("address ILIKE ?", "%#{params[:address]}%")
-    elsif params[:parcel]
-      @results = Property.where("parcel_number ILIKE ?", "%#{params[:parcel]}%")
-    elsif params[:year]
-      @results = Property.where("year_built ILIKE ?", "%#{params[:year]}%")
-    else
+    query = params[:query]
+    unless @results = Property.where("address ILIKE ? OR parcel_number = ? OR year_built = ?", "%#{query}%", "#{query}", "#{query}")
       @results = []
     end
+
+    # if params[:address]
+    #   @results = Property.where("address ILIKE ?", "%#{params[:address]}%")
+    # elsif params[:parcel]
+    #   @results = Property.where("parcel_number ILIKE ?", "%#{params[:parcel]}%")
+    # elsif params[:year]
+    #   @results = Property.where("year_built ILIKE ?", "%#{params[:year]}%")
+    # else
+    #   @results = []
+    # end
   end
 
 end
